708869a8a454d3e8ea491de15da765ff511a97e7
Harsh <harshrjjpt@gmail.com>
2022-06-01 11:44:01 +0530

    reading input and adding it to the html tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Harsh';
+  input = '';
+
   getName(value) {
     console.log(value);
+    this.input = value;
   }
 }
